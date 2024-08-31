36a78b3692aa7fa44a269253706ee32ac43d4a06
yusuf <yusufgad24@gmail.com>
2024-08-31 16:51:56 +0300

    HomeCategories section a shadcnui carousel ekledim

--- frontend/components/SliderButton.tsx
+++ frontend/components/SliderButton.tsx
@@ -1,14 +1,27 @@
 import clsx from "clsx";
 import { ReactNode } from "react";
 
-function SliderButton({ icon, active }: { icon: ReactNode; active?: boolean }) {
+function SliderButton({
+  icon,
+  active,
+  onClick,
+  disabled,
+}: {
+  icon: ReactNode;
+  active?: boolean;
+  onClick?: () => void;
+  disabled?: boolean;
+}) {
   return (
     <button
+      onClick={onClick}
+      disabled={disabled}
       className={clsx(
-        "flex h-10 w-10 items-center justify-center rounded-xl sm:h-12 sm:w-12 group ",
+        "flex h-10 w-10 items-center justify-center rounded-xl sm:h-12 sm:w-12 group",
         {
-          "bg-primary-800 text-white-5": active,
-          "bg-primary-10 text-primary-90": !active,
+          "bg-primary-800 text-white-5": active && !disabled,
+          "bg-primary-10 text-primary-90": !active && !disabled,
+          "bg-gray-300 text-gray-500 cursor-not-allowed": disabled,
         }
       )}
     >
--- frontend/components/layout/home/HomeCategories.tsx
+++ frontend/components/layout/home/HomeCategories.tsx
@@ -1,5 +1,14 @@
+"use client";
+
+import React, { useCallback, useState, useEffect } from "react";
 import Label from "@/components/Label";
 import SliderButton from "@/components/SliderButton";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  type CarouselApi,
+} from "@/components/ui/carousel";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 
 const categoriesList = [
@@ -11,46 +20,75 @@ const categoriesList = [
 
 function CategoryCard({ title, image }: { title: string; image: string }) {
   return (
-    <div className="relative col-span-1 mb-8 aspect-[9/11] overflow-hidden bg-gray-200  hover:border-tertiary-600 border transition-colors duration-100 cursor-pointer group ">
-      {/* category name */}
-      {/* <div className="absolute right-0 top-4 z-10">
-        <p className="text-7xl font-bold text-dark-80/50">
-        {title.split(" ")[0]}
-        </p>
-        </div> */}
-
-      {/* image */}
-      <div className="relative h-full w-full ">
-        <img className="h-full w-full object-cover" src={image} alt="man" />
+    <div className="relative col-span-1 mb-8 aspect-[9/11] overflow-hidden bg-gray-200 hover:border-tertiary-600 border transition-colors duration-100 cursor-pointer group">
+      <div className="relative h-full w-full">
+        <img className="h-full w-full object-cover" src={image} alt={title} />
       </div>
-
       <Label title={title} />
     </div>
   );
 }
 
 function HomeCategories() {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  const scrollPrev = useCallback(() => {
+    api?.scrollPrev();
+  }, [api]);
+
+  const scrollNext = useCallback(() => {
+    api?.scrollNext();
+  }, [api]);
+
   return (
     <section className="custom-container px-8 py-12">
       <div className="mb-8 sm:mb-12 flex items-center justify-between">
         <h2 className="text-lg font-medium sm:text-3xl">Popüler Kategoriler</h2>
 
         <div className="flex gap-2 md:gap-4">
-          <SliderButton icon={<ArrowLeft size={20} />} />
-          <SliderButton icon={<ArrowRight size={20} />} active={true} />
+          <SliderButton
+            onClick={scrollPrev}
+            icon={<ArrowLeft size={20} />}
+            active={current !== 1}
+          />
+          <SliderButton
+            onClick={scrollNext}
+            icon={<ArrowRight size={20} />}
+            active={current !== count}
+          />
         </div>
       </div>
 
-      <div className="grid grid-cols-2 gap-2 md:gap-8 lg:grid-cols-4">
-        {categoriesList.map((category, index) => (
-          <div
-            key={category.title}
-            className={`${index >= 2 ? "hidden lg:block" : ""} `}
-          >
-            <CategoryCard title={category.title} image={category.image} />
-          </div>
-        ))}
-      </div>
+      <Carousel
+        setApi={setApi}
+        opts={{
+          align: "start",
+        }}
+        className="w-full"
+      >
+        <CarouselContent>
+          {categoriesList.map((category, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/4">
+              <CategoryCard title={category.title} image={category.image} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </section>
   );
 }
